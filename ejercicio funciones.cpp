a58bce5e5a5f913f65ba33b22f241a222f556f23
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-10-17 12:37:51 -0600

    Add files via upload

---
+++ ejercicio funciones.cpp
@@ -0,0 +1,56 @@
+/*
+// primer ejercicio
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int suma(int, int);
+int main()
+{
+	int num1,num2, suma;
+	srand(time(NULL));
+	printf("Introducir dos numeros a sumar \n");
+	scanf("%d %d", &num1, &num2); 
+	suma = (num1 + num2);
+	
+	printf("La suma es: %d", suma);
+	return 0;
+}
+void suma(int *a, int *b );
+void suma(int*a, int*b)
+{
+	int tmp;
+	tmp = *a + *b;
+	
+}
+|*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	int numero,numero, 4;
+	
+	int perimetroFigura (float numero);
+	
+	float perimetro;
+	perimetro = 4 * numero;
+	return () perimetro;
+}
+void areaFigura(float numero,);
+{
+	float area;
+	area = numero * numero;
+	return area;
+}
+int main()
+{
+	int num;
+	printf("Lado de un cuadrado: ");
+	scanf("%d", %num);
+	printf("Perimetro: %d\n", perimetrofigura(num));
+	printf("Area: %d\n", areafigura(num));
+	return 0;
+}
+
