db093bf12479b05065c81f3243b0aa3a9e9a3c8d
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-10-17 13:08:05 -0600

    ejercicio funciones corregido

--- ejercicio funciones.cpp
+++ ejercicio funciones.cpp
@@ -1,56 +1,46 @@
 /*
 // primer ejercicio
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int suma(int, int);
-int main()
-{
-	int num1,num2, suma;
-	srand(time(NULL));
-	printf("Introducir dos numeros a sumar \n");
-	scanf("%d %d", &num1, &num2); 
-	suma = (num1 + num2);
-	
-	printf("La suma es: %d", suma);
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void suma(int, int);
+
+int main() {
+    int num1, num2;
+    srand(time(NULL));
+    printf("Introducir dos numeros a sumar \n");
+    scanf("%d %d", &num1, &num2);
+    suma(num1, num2);
+    return 0;
 }
-void suma(int *a, int *b );
-void suma(int*a, int*b)
-{
-	int tmp;
-	tmp = *a + *b;
-	
+
+void suma(int a, int b) {
+    int resultado = a + b;
+    printf("La suma es: %d\n", resultado);
 }
+
+
 |*/
+//ejercicio 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void suma(int, int);
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main()
-{
-	int numero,numero, 4;
-	
-	int perimetroFigura (float numero);
-	
-	float perimetro;
-	perimetro = 4 * numero;
-	return () perimetro;
+int main() {
+    int num1, num2;
+    srand(time(NULL));
+    printf("Introducir dos numeros a sumar \n");
+    scanf("%d %d", &num1, &num2);
+    suma(num1, num2);
+    return 0;
 }
-void areaFigura(float numero,);
-{
-	float area;
-	area = numero * numero;
-	return area;
+
+void suma(int a, int b) {
+    int resultado = a + b;
+    printf("La suma es: %d\n", resultado);
 }
-int main()
-{
-	int num;
-	printf("Lado de un cuadrado: ");
-	scanf("%d", %num);
-	printf("Perimetro: %d\n", perimetrofigura(num));
-	printf("Area: %d\n", areafigura(num));
-	return 0;
-}
+
 
